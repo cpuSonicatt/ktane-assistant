5e934178944159046b704fd902acfcb61a55f5c7
cpuSonicatt <18102974+ColSonicatt@users.noreply.github.com>
2021-08-11 21:25:34 +0100

    Ohana

---
+++ src/helpers/ButtonHelper.js
@@ -0,0 +1,44 @@
+import annyang from "annyang";
+import Speaker from "../speaker/Speaker";
+import Button from "ktane-solver/button"
+import { main } from "./MainHelper";
+
+const buttonColourTextCommand = {
+    ":colour :text": {"regexp": /(red|blue|yellow|black|white) ?(abort|hold|detonate|press)?/, "callback": getSolution1 }
+}
+
+const buttonColourCommand = {
+    ":colour": {"regexp": /(red|blue|yellow|black|white)/, "callback": getSolution2 }
+}
+let bomb;
+
+
+export function solveButton(x) {
+    bomb = x
+    Speaker.say("button")
+    annyang.removeCommands()
+    annyang.addCommands(buttonColourTextCommand)
+    annyang.debug(true) // remove me
+    annyang.start()
+}
+
+function getSolution1(colour, text) {
+    annyang.removeCommands()
+    let input = colour + " " + text
+    let solution = Button.solveStage1(input, bomb)
+    if (solution === "HOLD") {
+        Speaker.say("hold the button. what is the stripe colour?")
+        annyang.addCommands(buttonColourCommand)
+        return
+    } else if (solution === "RELEASE") {
+        Speaker.say("press and release the button")
+        main()
+    }
+}
+
+function getSolution2(colour) {
+    Speaker.say("release timer. " + Button.solveStage2(colour) + ". in any position")
+    annyang.removeCommands()
+    main()
+
+}
---
+++ src/helpers/MazeHelper.js
@@ -0,0 +1,58 @@
+import annyang from "annyang";
+import Speaker from "../speaker/Speaker";
+import Maze from "ktane-solver/maze"
+import { main } from "./MainHelper";
+
+let indicator
+let square
+let triangle
+
+const mazeCommands = {
+    "indicator :x :y": {"regexp": /indicator (1|2|3|4|5|6) ?(1|2|3|4|5|6)/, "callback": setIndicator},
+    "square :x :y": {"regexp": /square (1|2|3|4|5|6) ?(1|2|3|4|5|6)/, "callback": setSquare},
+    "triangle :x :y": {"regexp": /triangle (1|2|3|4|5|6) ?(1|2|3|4|5|6)/, "callback": setTriangle}
+}
+
+
+export function solveMaze() {
+    Speaker.say("maze")
+    awaitMaze()
+    annyang.removeCommands()
+    annyang.addCommands(mazeCommands)
+    annyang.debug(true) // remove me
+    annyang.start()
+}
+
+function awaitMaze() {
+    if (indicator == null || square == null || triangle == null) {
+        setTimeout(awaitMaze, 250)
+    } else {
+        Speaker.say("ready")
+        getSolution()
+        main()
+    }
+
+}
+
+function getSolution() {
+    let solution = Maze.solve(indicator, square, triangle)
+    for (let direction of solution) {
+        Speaker.say(direction)
+    }
+    return
+}
+
+function setIndicator(x, y) {
+    Speaker.say(`indicator ${x} ${y}`)
+    indicator = [x, y]
+}
+
+function setSquare(x, y) {
+    Speaker.say(`square ${x} ${y}`)
+    square = [x, y]
+}
+
+function setTriangle(x, y) {
+    Speaker.say(`triangle ${x} ${y}`)
+    triangle = [x, y]
+}
